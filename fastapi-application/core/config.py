5f434ed20f32eb137982252b3860696ea86e6722
Попков Ярослав <i.popkov@dssl.ru>
2024-09-09 21:22:47 +0300

    Added settings config

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -1,9 +1,9 @@
 from pydantic import BaseModel, PostgresDsn
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class RunConfig(BaseModel):
-    host: str = '0.0.0.0'
+    host: str = '127.0.0.1'
     port: int = 8000
 
 
@@ -20,6 +20,12 @@ class DatabaseConfig(BaseModel):
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".env.template", ".env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__"
+    )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
     db: DatabaseConfig
