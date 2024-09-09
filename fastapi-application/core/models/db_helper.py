bf24c5e28b378a11e73f61917736a1ec37be9f6e
Попков Ярослав <i.popkov@dssl.ru>
2024-09-09 21:22:58 +0300

    Added db_helper

--- fastapi-application/core/models/db_helper.py
+++ fastapi-application/core/models/db_helper.py
@@ -38,7 +38,7 @@ class DatabaseHelper:
 
 
 db_helper = DatabaseHelper(
-    url=settings.db.url,
+    url=str(settings.db.url),
     echo=settings.db.echo,
     echo_pool=settings.db.echo_pool,
     max_overflow=settings.db.max_overflow,
